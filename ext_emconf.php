5a0ce04add2138b365bb2cc97bf1a8bb4fa49bbc
Sven Jürgens <SvenJuergens@users.noreply.github.com>
2022-09-06 17:07:26 +0200

    [TASK] Update TYPO3 Dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.99-11.9.99'
+            'typo3' => '10.4.17-11.9.99'
         ],
         'conflicts' => [],
         'suggests' => [
