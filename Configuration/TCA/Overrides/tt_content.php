596e64c364f81d256dbcccc289212e34f7d6b28d
SvenJuergens <SvenJuergens@users.noreply.github.com>
2024-07-25 00:52:46 +0200

    TYPO3 Constants

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,6 +1,6 @@
 <?php
+defined('TYPO3') || die();
 
-defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Sup7even.Mailchimp',
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,6 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') || die();
+
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Mailchimp',
